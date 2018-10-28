8c19050193c3fc69106213f6c07bdbd86fb2e0c7
Nico Jones Gödel <jones.godel@gmail.com>
2018-10-28 16:46:52 +0100

    added build

---
+++ build/precache-manifest.c969f01a804d1926519e256a565b55cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fef4ff22744fbf8c5356",
+    "url": "/static/js/main.fef4ff22.chunk.js"
+  },
+  {
+    "revision": "424c1354542cc121c385",
+    "url": "/static/js/1.424c1354.chunk.js"
+  },
+  {
+    "revision": "fef4ff22744fbf8c5356",
+    "url": "/static/css/main.ff37fd22.chunk.css"
+  },
+  {
+    "revision": "09140798cdd09279210979f9598ba184",
+    "url": "/index.html"
+  }
+];
